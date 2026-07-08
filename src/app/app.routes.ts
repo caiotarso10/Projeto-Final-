1c8f64b89a66b421d47493235b89405a35f88eeb
caiotarso10 <caiotarso10@gmail.com>
2026-07-08 11:56:04 -0300

    adicionar rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+    path: "",
+    pathMatch: "full",
+    loadComponent:() => {
+    return import("./pages/login/login.component")
+    .then(c => c.LoginComponent)
+    }
+},
+
+{
+    path: "cadastro",
+    pathMatch: "full",
+    loadComponent:() => {
+    return import("./pages/cadastro/cadastro.component")
+    .then(c => c.CadastroComponent)
+    }
+},
+{
+    path: "catalogo",
+    pathMatch: "full",
+    loadComponent:() => {
+    return import("./pages/catalogo/catalogo.component")
+    .then(c => c.CatalogoComponent)
+    }
+},
+
+{
+    path: "home",
+    pathMatch: "full",
+    loadComponent:() => {
+    return import("./pages/home/home.component")
+    .then(c => c.HomeComponent)
+    }
+
+},
+
+{
+    path: "compra",
+    pathMatch: "full",
+    loadComponent:() => {
+    return import("./pages/compra/compra.component")
+    .then(c => c.CompraComponent)
+    }
+}
+
+];
--- src/app/pages/catalogo/catalogo.component.spec.ts
+++ src/app/pages/catalogo/catalogo.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ComponentsComponent } from './components.component';
+import { CatalogoComponent } from './catalogo.component';
 
-describe('ComponentsComponent', () => {
-  let component: ComponentsComponent;
-  let fixture: ComponentFixture<ComponentsComponent>;
+describe('CatalogoComponent', () => {
+  let component: CatalogoComponent;
+  let fixture: ComponentFixture<CatalogoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ComponentsComponent]
+      imports: [CatalogoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ComponentsComponent);
+    fixture = TestBed.createComponent(CatalogoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
